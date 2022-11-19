e277ecd5ee248f652117cc98af5462933266acd4
Brenda Rayane <brenda.rayane.0018@gmail.com>
2022-11-19 11:46:27 -0300

    rotas sign-in, sign-up e transacoes criadas

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import joi from "joi";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+import dayjs from "dayjs";
 
 //configs
 const app = express();
@@ -18,6 +20,12 @@ const userSchema = joi.object({
     password: joi.string().min(4).required()
 });
 
+const transacaoSchema = joi.object({
+    value: joi.number().required(),
+    description: joi.string().min(3).required(),
+    type: joi.string().valid("saída", "entrada").required()
+})
+
 const mongoClient = new MongoClient("mongodb://localhost:27017");
 
 try {
@@ -29,6 +37,8 @@ try {
 
 const db = mongoClient.db("ApiMyWallet");
 const collectionUsers = db.collection("users");
+const collectionSessions = db.collection("sessions");
+const collectionTransacoes = db.collection("transacoes");
 
 app.post("/sign-up", async (req, res) => {
     const bodyUser = req.body;
@@ -59,13 +69,100 @@ app.post("/sign-up", async (req, res) => {
 });
 
 
-// app.post("/sign-in", async (req, res) => {
+app.post("/sign-in", async (req, res) => {
+    const { email, password } = req.body;
 
-// })
+    const token = uuidV4();
 
-// app.get("/transacoes", async (req, res) => {
+    try {
+        const userExist = await collectionUsers.findOne({ email });
+        if (!userExist) {
+            return res.sendStatus(401);
+        }
 
-// })
+        const passwordOkay = bcrypt.compareSync(password, userExist.password);
+        if (!passwordOkay) {
+            return res.status(401).send("Senha incorreta!");
+        }
+
+        await collectionSessions.insertOne({
+            token,
+            userId: userExist._id
+        })
+
+        return res.status(201).send({ token });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send("Algum erro ocorreu!");
+    }
+
+})
+
+app.post("/transacoes", async (req, res) => {
+    const bodyTransacao = req.body
+    const { authorization } = req.headers;
+
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) {
+        return res.status(401).send("Token não enviado!");
+    }
+
+    const { error } = transacaoSchema.validate(bodyTransacao, { abortEarly: false });
+
+    if (error) {
+        const errors = error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const session = await collectionSessions.findOne({ token });
+        if (!session) {
+            return res.sendStatus(401);
+        }
+
+        const user = await collectionUsers.findOne({ _id: session.userId });
+        if (!user) {
+            return res.sendStatus(401);
+        }
+
+        const transacoesUser = await collectionTransacoes.insertOne({ ...bodyTransacao, time: dayjs().locale("pt").format("DD/MM/YYYY"), userId: user._id });
+
+        return res.status(201).send({message:"Transação feita com sucesso!"});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Algum erro ocorreu!");
+    }
+
+})
+
+app.get("/transacoes", async (req, res) => {
+    const { authorization } = req.headers;
+
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) {
+        return res.status(401).send("Token não enviado!");
+    }
+
+    try {
+        const session = await collectionSessions.findOne({ token });
+        
+        const user = await collectionUsers.findOne({ _id: session?.userId });
+        if (!user) {
+            return res.sendStatus(401);
+        }
+        
+        const transacoesUser = await collectionTransacoes.find({ userId: user._id }).toArray();
+
+        return res.status(201).send(transacoesUser)
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Algum erro ocoreu!")
+    }
+
+})
 
 // app.put("transacoes", async (req, res) => {
 
